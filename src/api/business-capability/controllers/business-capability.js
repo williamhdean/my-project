196036c18da4aa3f24e919a7d6225f7315d96c31
Billy Dean <billy@deanhome.net>
2022-10-29 07:22:03 -0500

    initial commit

---
+++ src/api/business-capability/controllers/business-capability.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * business-capability controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::business-capability.business-capability');
---
+++ src/api/business-capability/routes/business-capability.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * business-capability router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::business-capability.business-capability');
